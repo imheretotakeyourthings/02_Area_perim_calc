278f105a1cbdefd03c9dd05af01d5ea4504e6893
imheretotakeyourthings <101297540+imheretotakeyourthings@users.noreply.github.com>
2022-03-18 10:59:57 +1300

    Create 01 looping basic.py

---
+++ 01 looping basic.py
@@ -0,0 +1,17 @@
+from urllib import response
+
+
+valid = False
+while not valid:
+    response = float(input("enter a number"))
+
+    if response > 0:
+        valid = True
+    
+
+    else:
+        print("plwals enter a number more then zero")
+        
+
+    
+
